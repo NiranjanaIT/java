20426c8535992d931664e473d3d2bf2e3245a80b
NiranjanaIT <162598706+NiranjanaIT@users.noreply.github.com>
2025-08-04 23:21:43 +0530

    Update arr1.java

---
+++ arr1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class arr1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square matrix (n): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        System.out.println("Square matrix with diagonal elements as 1:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
