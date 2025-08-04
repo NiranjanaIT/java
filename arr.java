efd71f0ac57ab7de1b2e5d6f26da4458b26d18f0
NiranjanaIT <162598706+NiranjanaIT@users.noreply.github.com>
2025-08-04 23:22:24 +0530

    Update arr.java

---
+++ arr.java
@@ -0,0 +1,12 @@
+public class arr{
+    public static void main(String args []){
+        int[] arr={10,20,30,40,50};
+        int a=30;
+        if(a==arr[2]){  
+            System.out.println(arr[2]);
+        }
+        else{
+            System.out.println("Not present");
+        }
+    }
+}
